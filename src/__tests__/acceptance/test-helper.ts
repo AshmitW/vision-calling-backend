5bb4fbd3a8cca5b3bca22507b1bc72c47b7ad91c
Ashmit <samith.shetty3@gmail.com>
2023-04-17 14:17:07 +0100

    Initial commit

---
+++ src/__tests__/acceptance/test-helper.ts
@@ -0,0 +1,32 @@
+import {VisionCallingBackendApplication} from '../..';
+import {
+  createRestAppClient,
+  givenHttpServerConfig,
+  Client,
+} from '@loopback/testlab';
+
+export async function setupApplication(): Promise<AppWithClient> {
+  const restConfig = givenHttpServerConfig({
+    // Customize the server configuration here.
+    // Empty values (undefined, '') will be ignored by the helper.
+    //
+    // host: process.env.HOST,
+    // port: +process.env.PORT,
+  });
+
+  const app = new VisionCallingBackendApplication({
+    rest: restConfig,
+  });
+
+  await app.boot();
+  await app.start();
+
+  const client = createRestAppClient(app);
+
+  return {app, client};
+}
+
+export interface AppWithClient {
+  app: VisionCallingBackendApplication;
+  client: Client;
+}
