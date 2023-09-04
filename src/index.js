b90fd6417e0044d33bd96418807f1cf1fd06d7a7
Ashmit <samith.shetty3@gmail.com>
2023-09-04 20:55:26 +0530

    feat: auth module

---
+++ src/index.js
@@ -0,0 +1,29 @@
+'use strict'
+
+const mongoose = require('./services/mongoose')
+const app = require('./services/express')
+const nodemailer = require('nodemailer')
+const { host, port, username, password } = require('./config').transporter
+
+// start app and connect to database
+app.start()
+mongoose.connect()
+
+const transporter = nodemailer.createTransport({
+  host: host,
+  port: port,
+  auth: {
+    user: username,
+    pass: password
+  }
+})
+
+transporter.verify(function (error, success) {
+  if (error) {
+    console.log(error)
+  } else {
+    console.log('Server is ready to take our messages')
+  }
+})
+
+module.exports = app
---
+++ src/validations/user.validation.js
@@ -0,0 +1,20 @@
+'use strict'
+
+const Joi = require('joi')
+
+// User validation rules
+module.exports = {
+  create: {
+    body: {
+      email: Joi.string().email().required(),
+      password: Joi.string().min(6).max(128).required(),
+      name: Joi.string().max(128).required()
+    }
+  },
+  login: {
+    body: {
+      email: Joi.string().email().required(),
+      password: Joi.string().min(6).max(128).required()
+    }
+  }
+}
