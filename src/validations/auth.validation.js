71058224b18967198ea130ae2b255829b4a285c7
Ashmit <samith.shetty3@gmail.com>
2023-09-09 00:58:30 +0530

    fix: login validation

--- src/validations/auth.validation.js
+++ src/validations/auth.validation.js
@@ -15,7 +15,7 @@ module.exports = {
     body: {
       email: Joi.string().email().required(),
       password: Joi.string().min(6).max(128).required(),
-      fcmToken: Joi.string().min(6).max(512)
+      fcmToken: Joi.string()
     }
   }
 }
